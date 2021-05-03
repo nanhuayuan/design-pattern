4412c0866fb324e3d66b1ad6c184f9d863b43ec2
Administrator <2044233868@qq.com>
2021-05-03 10:00:31 +0800

    第一次提交

---
+++ src/test/java/com/poi/study/design/pattern/demo/DesignPatternDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poi.study.design.pattern.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
